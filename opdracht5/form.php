0430498c09268215e58b05be1b82427e5d1db5d7
carlijnmaria <57219902+carlijnmaria@users.noreply.github.com>
2019-11-05 10:18:49 +0100

    Opdrahct 5

---
+++ opdracht5/form.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlijn
+ * Date: 05/11/2019
+ * Time: 09:51
+ */
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form adding ape </title>
+</head>
+<body>
+
+<form method = "post" action ="process.php">
+    Species: <input type="text" name="soort" placeholder="Add your species here">
+    <br>
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
